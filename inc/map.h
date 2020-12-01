0f91a47a9fd84f6a06627027001d6047e15a88bf
Zhong_Ming <a18786717618@126.com>
2020-12-01 23:56:47 +0800

    Add basic comments

--- inc/map.h
+++ inc/map.h
@@ -1,36 +1,66 @@
 #ifndef __MAP_H
 #define __MAP_H
 
+//-------------------------------------------------------------------
+// Map Block Type Macros
+//-------------------------------------------------------------------
 #define MINE (9)
 #define EXPLODED_MINE (10)
 #define EMPTY ('0')
 
-#define MAX_TIMES (10000)
+//-------------------------------------------------------------------
+// Map Block Manipulate Macros
+//-------------------------------------------------------------------
+#define set_mine(y, x, map) map->arr[y][x] = MINE
+#define set_exploded_mine(y, x, map) map->arr[y][x] = EXPLODED_MINE
+#define open_block(y, x, map) map->arr[y][x] += '0'
+#define set_num(y, x, map, n) map->arr[y][x] = n
+#define get_block(y, x, map) map->arr[y][x]
 
+//-------------------------------------------------------------------
+// Map Block Status Macros
+//-------------------------------------------------------------------
 #define has_mine(y, x, map) (map->arr[y][x] == MINE)
 #define in_range(y, x, up, left, down, right) (y >= up && x >= left && y < down && x < right)
 #define in_map_range(y, x, map) in_range(y, x, 0, 0, map->col, map->row)
 #define is_empty(y, x, map) (map->arr[y][x] == EMPTY)
 #define is_shown(y, x, map) (map->arr[y][x] >= '0' && map->arr[y][x] <= '9')
 
-/* The biggest char in map is '9'(00111001),
- * so I set flag in sixth bit. (start from zero)
+/**
+ * @name The position of flag bit in map block
+ * 
+ * @note The biggest char in map is '9'(00111001),
+ *       so I set flag in sixth bit. (start from zero)
  */
 #define FLAG_BIT 6
-#define set_flag(n) ((n) |= (1 << FLAG_BIT))
-#define has_flag(n) ((n) & (1 << FLAG_BIT))
-#define unset_flag(n) ((n) &= ~(1 << FLAG_BIT))
+//-------------------------------------------------------------------
+// Flag Manipulate Macros
+//-------------------------------------------------------------------
+#define set_flag(y, x, map) ((map->arr[y][x]) |= (1 << FLAG_BIT))
+#define has_flag(y, x, map) ((map->arr[y][x]) & (1 << FLAG_BIT))
+#define unset_flag(y, x, map) ((map->arr[y][x]) &= ~(1 << FLAG_BIT))
 
-typedef struct _map{
-    char **arr;
-    unsigned short col, row;
-} * Map;
+//-------------------------------------------------------------------
+// Type Definations
+//-------------------------------------------------------------------
+/**
+ * @brief The struct used to record map status.
+ */
+struct _map {
+    char **arr;                 ///< The two dimension array.
+    unsigned short col, row;    ///< The column and row of the map.
+};
+typedef struct _map* Map;
 
+//-------------------------------------------------------------------
+// Prototypes
+//-------------------------------------------------------------------
 Map create_map(unsigned short col, unsigned short row);
 void put_mines(Map map, unsigned short num);
 void remove_mine(Map map, short y, short x);
 unsigned short cnt_mines(Map map, short y, short x);
 unsigned short cnt_flags(Map map, short y, short x);
-void destroy_map(Map *p_m);
+void clear_map(Map map);
+void destroy_map(Map map);
 
 #endif
--- src/map.c
+++ src/map.c
@@ -1,27 +1,45 @@
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 #include "map.h"
 #include "fatal.h"
 
+#define FAILED_PLACEMENT_MAX_TIMES (10000)
+
 const short directions[8][2] = {{-1, -1}, {-1, 0}, {0, -1}, {1, 1}, {1, 0}, {0, 1}, {1, -1}, {-1, 1}};
 
+/**
+ * @brief Create a empty square map with specified size.
+ * 
+ * @param col The column of the map.
+ * @param row The row of the map.
+ * 
+ * @return The created map.
+ * 
+ * @warning The param col and row can't be zero.
+ */
 Map create_map(unsigned short col, unsigned short row)
 {
     Map new_map = NULL;
     
-    if ((new_map = malloc(sizeof(struct _map))) == NULL)
-            FatalError("Out of space");
-    if ((new_map->arr = malloc(col * sizeof(char *))) == NULL)
-            FatalError("Out of space!");
+    new_map = malloc_fatal(sizeof(struct _map), "create_map - new_map");
+    new_map->arr = malloc_fatal(col * sizeof(char *), "create_map - new_map->arr");
     for (unsigned short i = 0; i < col; i++)
-        if ((new_map->arr[i] = calloc(row, sizeof(char))) == NULL)
-            FatalError("Out of space!");
+        new_map->arr[i] = calloc_fatal(row, sizeof(char), "create_map - new_map->arr[i]");
     
     new_map->col = col;
     new_map->row = row;
     return new_map;
 }
 
+/**
+ * @brief Put a certain number of mines on a map.
+ * 
+ * @param map The map to place mines.
+ * @param num The number of mines to put.
+ * 
+ * @warning If tried to place mines too many times, the program will exit.
+ */
 void put_mines(Map map, unsigned short num)
 {
     short y, x;
@@ -34,7 +52,7 @@ void put_mines(Map map, unsigned short num)
             y = rand() % map->col;
             x = rand() % map->row;
             times++;
-            if (times > MAX_TIMES)
+            if (times > FAILED_PLACEMENT_MAX_TIMES)
                 Error("Tried too many times");
         } while(has_mine(y, x, map));
         
@@ -49,10 +67,22 @@ void put_mines(Map map, unsigned short num)
     }
 }
 
+/**
+ * @brief Remove the (y, x) mines in the map.
+ * 
+ * @param map The map for which has the specified mine.
+ * @param y   The column number of the mine.
+ * @param x   The row number of the mine.
+ * 
+ * @note If there is no mine at (y, x), the program will exit.
+ */
 void remove_mine(Map map, short y, short x)
 {
     if (!has_mine(y, x, map))
+    {
         fprintf(stderr, "No mine in (%hd, %hd)", y, x);
+        exit(1);
+    }
     for (int i = 0; i < 8; i++)
     {
         short next_y = y + directions[i][0];
@@ -62,6 +92,13 @@ void remove_mine(Map map, short y, short x)
     }
 }
 
+/**
+ * @brief Count the number of mines arount (i, j).
+ * 
+ * @param map The map for which has the specified position.
+ * @param y   The column number of the position.
+ * @param x   The row number of the position.
+ */
 unsigned short cnt_mines(Map map, short y, short x)
 {
     unsigned short cnt = 0;
@@ -75,6 +112,13 @@ unsigned short cnt_mines(Map map, short y, short x)
     return cnt;
 }
 
+/**
+ * @brief Count the number of flags arount (i, j).
+ * 
+ * @param map The map for which has the specified position.
+ * @param y   The column number of the position.
+ * @param x   The row number of the position.
+ */
 unsigned short cnt_flags(Map map, short y, short x)
 {
     unsigned short cnt = 0;
@@ -82,21 +126,37 @@ unsigned short cnt_flags(Map map, short y, short x)
     {
             short next_y = y + directions[i][0];
             short next_x = x + directions[i][1];
-            if(in_map_range(next_y, next_x, map) && has_flag(map->arr[next_y][next_x]))
+            if(in_map_range(next_y, next_x, map) && has_flag(next_y, next_x, map))
                 cnt++;
     }
     return cnt;
 }
 
-void destroy_map(Map *p_m)
+/**
+ * @brief Clear the map.
+ * 
+ * @param map The map to clear.
+ * 
+ */
+void clear_map(Map map)
 {
-    for (unsigned short i = 0; i < (*p_m)->col; i++)
+    for (unsigned int i = 0; i < map->col; i++)
+        memset(map->arr[i], 0, map->row);
+}
+
+/**
+ * @brief Destroy the map.
+ * 
+ * @param map The map to destory.
+ */
+void destroy_map(Map map)
+{
+    for (unsigned short i = 0; i < map->col; i++)
     {
-        free((*p_m)->arr[i]);
-        (*p_m)->arr[i] = NULL;
+        free(map->arr[i]);
+        map->arr[i] = NULL;
     }
-    free((*p_m)->arr);
-    (*p_m)->arr = NULL;
-    free(*p_m);
-    *p_m = NULL;
+    free(map->arr);
+    map->arr = NULL;
+    free(map);
 }
