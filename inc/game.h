b97c2e4f3e0d4ca5aaf0bce59c009f13f998dd17
Zhong_Ming <a18786717618@126.com>
2020-12-03 20:33:29 +0800

    Add Game module, add comments

--- inc/game.h
+++ inc/game.h
@@ -1,9 +1,23 @@
+/**
+ * @file game.h
+ * @author jkilopu
+ * @brief The core elements of Mines.
+ */
+
 #ifndef __GAME_H
 #define __GAME_H
 
 #include "map.h"
+#include "timer.h"
 #include <stdbool.h>
 
+//-------------------------------------------------------------------
+// Type Definations
+//-------------------------------------------------------------------
+
+/**
+ * @brief All settings in mymines.
+ */
 typedef struct _settings {
     unsigned short map_width, map_height;
     unsigned short n_mine;
@@ -11,19 +25,34 @@ typedef struct _settings {
     unsigned short block_size;
 } Settings;
 
-void setup(void);
-Map start(void);
+/**
+ * @brief All elements in game.
+ */
+typedef struct _game {
+    Map map;
+    Settings settings;
+    Timer timer;
+    unsigned short opened_blocks;
+} * Game;
 
-void show_unknown(unsigned short col, unsigned short row);
-void show_mines(Map map);
-bool click_map(Map map, short y, short x, bool *first_click);
-void show_block(Map map, short y, short x);
-void set_draw_flag(Map map, unsigned short y, unsigned short x);
-bool open_with_flag(Map map, unsigned short y, unsigned short x);
-bool success(void);
+//-------------------------------------------------------------------
+// Prototypes
+//-------------------------------------------------------------------
 
-void finish(Map map);
-void restart(Map map);
-void wrapup(Map map);
+Game setup(void);
+void create_map_in_game(Game game);
+
+bool click_map(Game game, short y, short x, bool *first_click);
+void set_draw_flag(Game game, unsigned short y, unsigned short x);
+static void show_blocks(Game game, short y, short x);
+static void show_unknown(Map map);
+static void show_mines(Map map);
+static bool open_with_flag(Game game, unsigned short y, unsigned short x);
+
+bool success(Game game);
+static void destroy_game(Game game);
+void finish(Game game);
+void restart(Game game);
+void wrapup(Game game);
 
 #endif
