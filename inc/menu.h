4343ac29d85981d9fe991a2b3964efd4be0d7064
Zhong_Ming <a18786717618@126.com>
2020-12-03 20:34:49 +0800

    Add comments

--- inc/menu.h
+++ inc/menu.h
@@ -1,3 +1,8 @@
+/**
+ * @file menu.h
+ * @author jkilopu
+ * @brief The menu with buttons and options.
+ */
 #ifndef __MENU_H
 #define __MENU_H
 
@@ -5,8 +10,12 @@
 #include "game.h"
 #include "button.h"
 
-void get_settings(Settings *p_s);
+//-------------------------------------------------------------------
+// Prototypes
+//-------------------------------------------------------------------
+
+void show_menu_and_get_settings(Settings *p_s);
 static void draw_menu(Digit ds[], Button bs[], int num);
-static void change_digits(Digit ds[], Button bs[], int num);
+static void menu_main(Digit ds[], Button bs[], int num);
 
 #endif
