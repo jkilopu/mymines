a632aabe2c2a6282138efbcb4ed5b8ff32815b53
Zhong_Ming <a18786717618@126.com>
2020-12-03 20:35:15 +0800

    Add Timer module, add comments

--- inc/timer.h
+++ inc/timer.h
@@ -6,10 +6,28 @@
 #define INTERVAL (1000)
 #define TIME_REGION_WIDTH (100)
 
-void set_timer(void);
-void unset_timer(void);
+//-------------------------------------------------------------------
+// Type Definations
+//------------------------------------------------------------------
+
+/**
+ * @brief Defination of mymines timer, with position and SDL2 timer.
+ */
+typedef struct _timer {
+    SDL_TimerID timer_id;
+    unsigned short time_passed; ///< Time in seconds
+    unsigned short time_block_x;
+    unsigned short time_block_y;
+} Timer;
+
+//-------------------------------------------------------------------
+// Prototypes
+//-------------------------------------------------------------------
+
+void set_timer(Timer *p_timer);
+void set_timer_pos(Timer *p_timer, unsigned short win_x, unsigned short win_y);
+void unset_timer(Timer *p_timer);
+void draw_timer(Timer *p_timer);
 Uint32 timer_callback(Uint32 interval, void *param);
-void draw_timer(void);
-void set_timer_pos(unsigned short win_x, unsigned short win_y);
 
 #endif
--- src/timer.c
+++ src/timer.c
@@ -3,55 +3,86 @@
 #include "render.h"
 #include "fatal.h"
 
-static SDL_TimerID timer_id;
-static unsigned short time_passed;
-static unsigned short show_time_x;
-static unsigned short show_time_y;
 extern SDL_Renderer *main_renderer;
-extern SDL_Texture *block_texture[];
+extern SDL_Texture *block_textures[];
 
-void set_timer(void)
+//-------------------------------------------------------------------
+// Functions
+//-------------------------------------------------------------------
+
+/**
+ * @brief Start timing.
+ * 
+ * @param p_timer The timer to set up.
+ */
+void set_timer(Timer *p_timer)
 {
-    if(!(timer_id = SDL_AddTimer(INTERVAL, timer_callback, NULL)))
+    if(!(p_timer->timer_id = SDL_AddTimer(INTERVAL, timer_callback, p_timer)))
         SDL_FatalError("Can't set timer!\n%s\n", SDL_GetError());
 }
 
-void unset_timer(void)
+/**
+ * @brief Set the timer block pos.
+ * 
+ * @param p_timer The timer.
+ */
+void set_timer_pos(Timer *p_timer, unsigned short win_x, unsigned short win_y)
 {
-    if (!SDL_RemoveTimer(timer_id))
-        Error("Timer not exist.");
-    time_passed = 0;
+    p_timer->time_block_x = win_x - TIME_REGION_WIDTH + TIME_REGION_WIDTH / 4;
+    p_timer->time_block_y = win_y / 3;
 }
 
+/**
+ * @brief Stop timming.
+ * 
+ * @param p_timer The timer.
+ */
+void unset_timer(Timer *p_timer)
+{
+    if (!SDL_RemoveTimer(p_timer->timer_id))
+        Error("Timer not exist.");
+    p_timer->time_passed = 0;
+}
+
+/**
+ * @brief The fucntion is called when
+ * 
+ * @param interval The current timer interval.
+ * @param param The param specified in "SDL_AddTimer".
+ * 
+ * @return The next timer interval.
+ */
 Uint32 timer_callback(Uint32 interval, void *param)
 {
-    if (time_passed > 99 * 60 + 60)
+    Timer *p_timer = (Timer *) param;
+    if (p_timer->time_passed > 99 * 60 + 60)
         return interval;
-    draw_timer();
-    time_passed++;
+    draw_timer(p_timer);
+    p_timer->time_passed++;
     return interval;
 }
 
-void draw_timer(void)
+/**
+ * @brief Draw the timer with the "main_renderer".
+ * 
+ * @param timer The timer to draw.
+ * 
+ * @warning This function can only be used after "set_timer" and "set_timer_pos" is called.`
+*/
+void draw_timer(Timer *p_timer)
 {
-    unsigned short mins_lo = (time_passed / 60) % 10;
-    unsigned short secs_lo = (time_passed % 60) % 10;
-    unsigned short mins_hi = (time_passed / 60) / 10;
-    unsigned short secs_hi = (time_passed % 60) / 10;
-    unsigned short block_size = (TIME_REGION_WIDTH < show_time_y ? TIME_REGION_WIDTH : show_time_y) / 3;
-    SDL_Rect dst_r = {show_time_x, show_time_y, block_size, block_size};
-    draw(main_renderer, block_texture[mins_hi], NULL, &dst_r);
+    unsigned short mins_lo = (p_timer->time_passed / 60) % 10;
+    unsigned short secs_lo = (p_timer->time_passed % 60) % 10;
+    unsigned short mins_hi = (p_timer->time_passed / 60) / 10;
+    unsigned short secs_hi = (p_timer->time_passed % 60) / 10;
+    unsigned short block_size = (TIME_REGION_WIDTH < p_timer->time_block_x ? TIME_REGION_WIDTH : p_timer->time_block_y) / 3;
+    SDL_Rect dst_r = {p_timer->time_block_x, p_timer->time_block_y, block_size, block_size};
+    draw(main_renderer, block_textures[mins_hi], NULL, &dst_r);
     dst_r.y += block_size + block_size / 4;
-    draw(main_renderer, block_texture[mins_lo], NULL, &dst_r);
+    draw(main_renderer, block_textures[mins_lo], NULL, &dst_r);
     dst_r.y += block_size + block_size / 3;
-    draw(main_renderer, block_texture[secs_hi], NULL, &dst_r);
+    draw(main_renderer, block_textures[secs_hi], NULL, &dst_r);
     dst_r.y += block_size + block_size / 4;
-    draw(main_renderer, block_texture[secs_lo], NULL, &dst_r);
+    draw(main_renderer, block_textures[secs_lo], NULL, &dst_r);
     SDL_RenderPresent(main_renderer);
-}
-
-void set_timer_pos(unsigned short win_x, unsigned short win_y)
-{
-    show_time_x = win_x - TIME_REGION_WIDTH + TIME_REGION_WIDTH / 4;
-    show_time_y = win_y / 3;
 }
