15de3c7a1506b182f1f749baba7d740c4e1cf9f8
Zhong_Ming <a18786717618@126.com>
2020-12-03 20:31:51 +0800

    Add comments, make block image paths a variable

--- src/block.c
+++ src/block.c
@@ -1,21 +1,65 @@
+/**
+ * @file block.c
+ * @author jkilopu
+ * @brief Provide block image paths and fuctions to draw block.
+ * 
+ * TODO: Aribitary origin of coordinates.
+ */
 #include "block.h"
 #include "render.h"
 
+const char *block_image_paths[BLOCK_TEXTURE_NUM] = {
+    "res/Background.gif",
+    "res/one.gif",
+    "res/two.gif",
+    "res/three.gif",
+    "res/four.gif",
+    "res/five.gif",
+    "res/six.gif",
+    "res/seven.gif",
+    "res/eight.gif",
+    "res/mine.gif",
+    "res/explodedMine.gif",
+    "res/ask.gif",
+    "res/flag.gif",
+    "res/hidden.gif",
+};
+SDL_Texture *block_textures[BLOCK_TEXTURE_NUM];
 static unsigned short block_size;
-SDL_Texture *block_texture[TEXTURE_NUM];
 extern SDL_Renderer *main_renderer;
 
+/**
+ * @brief Set the block size.
+ * 
+ * @param bs The block size.
+ * 
+ * @note The functions must be called before any other functions in the block module.
+*/
 void set_block_size(unsigned short bs)
 {
     block_size = bs;
 }
 
-/* Draw block according to the position in map */
+/**
+ * @brief Draw block according to the position.
+ * 
+ * @param b The block enum.
+ * @param y The yth block pos on y axis.
+ * @param x The xth block pos on x axis.
+*/
 void draw_block(BLOCK b, unsigned short y, unsigned short x)
 {
-    draw_block_helper(main_renderer, block_texture[b], y, x);
+    draw_block_helper(main_renderer, block_textures[b], y, x);
 }
 
+/**
+ * @brief The helper function of "draw_block".
+ * 
+ * @param r The renderer.
+ * @param t The texture to draw.
+ * @param y The yth block pos on y axis.
+ * @param x The xth block pos on x axis.
+ */
 static void draw_block_helper(SDL_Renderer *r, SDL_Texture *t, unsigned short y, unsigned short x)
 {
     SDL_Rect dst_r = {
@@ -27,9 +71,14 @@ static void draw_block_helper(SDL_Renderer *r, SDL_Texture *t, unsigned short y,
     draw(r, t, NULL, &dst_r);
 }
 
-/* Conver window pos to map pos */
-void window2map(short *y, short *x)
+/**
+ * @brief Convert window pos to map pos.
+ * 
+ * @param p_y The pointer to window pos on y axis.
+ * @param p_x The pointer to window pos on x axis.
+ */
+void window2map(unsigned short *p_y, unsigned short *p_x)
 {
-    *y = *y / block_size;
-    *x = *x / block_size;
+    *p_y = *p_y / block_size;
+    *p_x = *p_x / block_size;
 }
