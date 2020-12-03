dfa9d84b8321e64b5cab3bb4a7cf36933702ca1e
Zhong_Ming <a18786717618@126.com>
2020-12-03 20:32:55 +0800

    Add comments

--- inc/fatal.h
+++ inc/fatal.h
@@ -1,8 +1,17 @@
+/**
+ * @file fatal.h
+ * @author jkilopu
+ * @brief Provide error handle methods which can simplify error handling.
+ */
 #ifndef __FATAL_H
 #define __FATAL_H
 
 #include <stdio.h>
 #include <stdlib.h>
+
+//-------------------------------------------------------------------
+// Error Handle Macros
+//-------------------------------------------------------------------
 
 #define Error(str) FatalError(str)
 #define FatalError(str) fprintf(stderr, "%s\n", str), exit(1)
@@ -10,8 +19,15 @@
 #define SDL_FatalError(...) fprintf(stderr, __VA_ARGS__), exit(1)
 #define SDL_Error(...) SDL_FatalError(__VA_ARGS__)
 
+//-------------------------------------------------------------------
+// Error Message Macros
+//-------------------------------------------------------------------
 #define MALLOC_FAIL_MSG "Malloc Failed!!!"
 #define CALLOC_FAIL_MSG "Calloc Failed!!!"
+
+//-------------------------------------------------------------------
+// Prototypes
+//-------------------------------------------------------------------
 
 void *malloc_fatal(size_t size, const char *location);
 void *calloc_fatal(size_t num, size_t size, const char *location);
