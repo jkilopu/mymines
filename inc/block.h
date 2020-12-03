11c881cebae8bc94cb6ff4548a7ea5fc497eadbf
Zhong_Ming <a18786717618@126.com>
2020-12-03 20:29:44 +0800

    Add comments, change a function prototype

--- inc/block.h
+++ inc/block.h
@@ -1,9 +1,14 @@
+/**
+ * @file block.h
+ * @author jkilopu
+ * @brief Provide "drawable" blocks (not blocks in Map), wrapped draw method and block resources.
+ */
 #ifndef __BLOCK_H
 #define __BLOCK_H
 
 #include "SDL.h"
 
-#define TEXTURE_NUM 14
+#define BLOCK_TEXTURE_NUM 14
 
 typedef enum _block
 {
@@ -23,9 +28,13 @@ typedef enum _block
     T_HIDDEN,
 } BLOCK;
 
+//-------------------------------------------------------------------
+// Prototypes
+//-------------------------------------------------------------------
+
 void set_block_size(unsigned short bs);
 void draw_block(BLOCK b, unsigned short y, unsigned short x);
 static void draw_block_helper(SDL_Renderer *r, SDL_Texture *t, unsigned short y, unsigned short x);
-void window2map(short *y, short *x);
+void window2map(unsigned short *p_y, unsigned short *p_x);
 
 #endif
