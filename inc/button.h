9e37c3e70fa936da7df65eb6cba5b5bf665e3338
Zhong_Ming <a18786717618@126.com>
2020-12-03 20:31:59 +0800

    Add comments

--- inc/button.h
+++ inc/button.h
@@ -1,36 +1,58 @@
+/**
+ * @file button.h
+ * @author jkilopu
+ * @brief Header only module. Provied type defination and manipulate macros of "Digit" and "Button".
+ * 
+ * @details About Button and Digit in mymines:
+ * A pair of buttons and digits in mymines look like this:
+ * ****        (The increase button rect)
+ * *  *
+ * ****
+ *             (This line is the interval)
+ * ****         Digit rect (to be changed by buttons)
+ * * 1*
+ * ****
+ *             (This line is the interval)
+ * ****
+ * *  *
+ * ****        (The down button rect)
+ * 
+ */
+
 #ifndef __BUTTON_H
 #define __BUTTON_H
 
 #include <stdint.h>
 #include "SDL.h"
 
+//-------------------------------------------------------------------
+// Digit Manipulate Macros
+//-------------------------------------------------------------------
+
 #define add_digit(p_x) ((*p_x) == 9 ? (*p_x) = 0 : (*p_x)++)
 #define reduce_digit(p_x) ((*p_x) == 0 ? (*p_x) = 9 : (*p_x)--)
 
-/*
- * The button here is like this:
- * ****        (The up button rect)
- * *  *
- * ****
- *             (This line is the interval)
- * digit_rect  (to be changed)
- * 
- * ****
- * *  *
- * ****        (The down button rect)
- * 
+//-------------------------------------------------------------------
+// Type Definations
+//-------------------------------------------------------------------
+
+/**
+ * @brief Button on the window that is used to change game data, pressed by user.
  */
 typedef struct _button
 {
     SDL_Rect r;
-    unsigned short y_interval;
-    uint8_t *p_data; // I only want to control a small part of data.
+    unsigned short y_interval; ///< The y interval between buttons.
+    uint8_t *p_data; ///< To change the data.
 } Button;
 
+/**
+ * @brief Digit on the window that is used to record game data.
+ */
 typedef struct _digit
 {
     SDL_Rect r;
-    uint8_t data;
+    uint8_t data; ///< The game data.
 } Digit;
 
 #endif
