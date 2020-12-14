e1058611e2639c58126f8e4bb5d0be950f028d4d
Zhong_Ming <a18786717618@126.com>
2020-12-14 21:04:07 +0800

    Lan mode support: 1. Fix packet size (Settings) 2. Function to handle lan activity 3. Use SDL_bool instead of bool

--- inc/game.h
+++ inc/game.h
@@ -9,7 +9,7 @@
 
 #include "map.h"
 #include "timer.h"
-#include <stdbool.h>
+#include "SDL_stdinc.h"
 
 //-------------------------------------------------------------------
 // Type Definations
@@ -18,11 +18,12 @@
 /**
  * @brief All settings in mymines.
  */
-typedef struct _settings {
-    unsigned short map_width, map_height;
-    unsigned short n_mine;
-    unsigned short window_height, window_width;
-    unsigned short block_size;
+typedef struct {
+    Uint16 map_width, map_height;
+    Uint16 n_mine;
+    Uint16 window_height, window_width;
+    Uint16 block_size;
+    Uint8 game_mode;
 } Settings;
 
 /**
@@ -36,20 +37,36 @@ typedef struct _game {
 } * Game;
 
 //-------------------------------------------------------------------
+// Game Mode Macros
+//-------------------------------------------------------------------
+
+#define LAN_LOCAL_BIT 0
+#define SERVER_CLIENT_BIT 1
+#define set_lan_mode(game_mode) (game_mode |= (1 << LAN_LOCAL_BIT))
+#define set_local_mode(game_mode) (game_mode &= ~(1 << LAN_LOCAL_BIT))
+#define set_server_mode(game_mode) (game_mode |= (1 << SERVER_CLIENT_BIT))
+#define set_client_mode(game_mode) (game_mode &= ~(1 << SERVER_CLIENT_BIT))
+#define is_lan_mode(game_mode) (game_mode & (1 << LAN_LOCAL_BIT))
+#define is_server_mode(game_mode) (game_mode & (1 << SERVER_CLIENT_BIT))
+
+//-------------------------------------------------------------------
 // Prototypes
 //-------------------------------------------------------------------
 
-Game setup(void);
+Game setup(int argc);
+static Game create_game_with_mode(Uint8 game_mode);
+void connect_and_complete_setup(Game game);
 void create_map_in_game(Game game);
 
-bool click_map(Game game, short y, short x, bool *first_click);
+SDL_bool handle_recved_packet(Game game, SDL_bool *first_click);
+SDL_bool click_map(Game game, unsigned short y, unsigned short x, SDL_bool *first_click);
 void set_draw_flag(Game game, unsigned short y, unsigned short x);
-static void show_blocks(Game game, short y, short x);
+static void show_blocks(Game game, unsigned short y, unsigned short x);
 static void show_unknown(Map map);
 static void show_mines(Map map);
-static bool open_with_flag(Game game, unsigned short y, unsigned short x);
+static SDL_bool open_with_flag(Game game, unsigned short y, unsigned short x);
 
-bool success(Game game);
+SDL_bool success(Game game);
 static void destroy_game(Game game);
 void finish(Game game);
 void restart(Game game);
