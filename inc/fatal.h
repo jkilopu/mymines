618ace53ae9f4f0d7d9e4a302663bf334635de8e
Zhong_Ming <a18786717618@126.com>
2020-12-14 21:02:23 +0800

    Use SDL_Log module

--- inc/fatal.h
+++ inc/fatal.h
@@ -7,18 +7,23 @@
 #ifndef __FATAL_H
 #define __FATAL_H
 
+#include "SDL_Log.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
+
+enum {
+    SDL_LOG_CATEGORY_NET = SDL_LOG_CATEGORY_CUSTOM,
+};
 
 //-------------------------------------------------------------------
 // Error Handle Macros
 //-------------------------------------------------------------------
 
-#define Error(...) fprintf(output, __VA_ARGS__), exit(1)
+#define Error(...) SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, __VA_ARGS__), exit(1)
 #define FatalError(...) Error(__VA_ARGS__)
-#define SDL_output_error(...) Error(__VA_ARGS__)
-#define SDL_output_fatal_error(...) SDL_output_error(__VA_ARGS__)
+#define SDL_other_fatal_error(...) Error(__VA_ARGS__)
+#define SDL_net_error(...) SDL_LogError(SDL_LOG_CATEGORY_NET, __VA_ARGS__), exit(1)
+#define SDL_render_fatal_error(...) SDL_LogError(SDL_LOG_CATEGORY_RENDER, __VA_ARGS__), exit(1)
 
 //-------------------------------------------------------------------
 // Error Message Macros
@@ -31,9 +36,7 @@
 // Prototypes
 //-------------------------------------------------------------------
 
-void set_fatal_output_stream(bool is_error_log_file);
 void *malloc_fatal(size_t size, const char *location);
 void *calloc_fatal(size_t num, size_t size, const char *location);
-void close_fatal_output_stream(void);
 
 #endif
